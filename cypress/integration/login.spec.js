4b2590453d96e548f941feacbd4c86c21f6208ec
Thiago Leviski <thiago_leviski@yahoo.com.br>
2022-12-10 19:20:40 -0300

    Pipeline

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -18,21 +18,21 @@ context('Funcionalidade Login', () => {
 
     it('Login com sucesso usando Comando customizado', () => {
         cy.login(dadosLogin.usuario, dadosLogin.senha)
-        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('[class="page-title"]').should('contain', 'Minha conta')
     });
 
     it('Login usando fixture', () => {
         cy.fixture('perfil').then((dados) => {
             cy.login(dados.usuario, dados.senha)
         })
-        cy.get('.page-title').should('contain', 'Minha conta')
+        cy.get('[class="page-title"]').should('contain', 'Minha conta')
     });
 
     it('Deve fazer login com sucesso - sem otimização', () => {
         cy.get('#username').type(dadosLogin.usuario)
         cy.get('#password').type(dadosLogin.senha, { log: false })
         cy.get('.woocommerce-form > .button').click()
-        cy.get('.page-title').should('contain', 'Minha conta')
-        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain', 'Olá, aluno_ebac')
+        cy.get('[class="page-title"]').should('contain', 'Minha conta')
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2) > :nth-child(1)').should('contain', 'aluno.ebac.159951')
     })
 })
